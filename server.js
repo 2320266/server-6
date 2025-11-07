1f0428dba513ad7c0e16fb4348a834fbae78d8e0
232-266 <206805069+2320266@users.noreply.github.com>
2025-11-07 11:45:28 +0900

    1

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 // 今回はクライアントサイドもルーティングもないので何もimportしない
+
+const kv = await Deno.openKv();
+
+//セット
+const pw = await kv.get(['dog', 'いぬ'], {power: 10, speed: 5});
+
+//ゲット
+const dog = await kv.get(['dog', 'いぬ']);
+console.log('power:' + dog.value);
+console.log('value:' + JSON.stringify(dog.value))
+console.log('key:' + dog.key);
+console.log(dog.value, dog.key);
+
+const kerai = await kv.get(['setvant'])
+console.log('dog:' + JSON.stringify(kerai.value))
