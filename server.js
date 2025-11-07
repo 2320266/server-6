2937c0f3df057de5d9eb5e734d48a482cb6d4fbe
232-266 <206805069+2320266@users.noreply.github.com>
2025-11-07 12:06:27 +0900

    2

--- server.js
+++ server.js
@@ -3,14 +3,36 @@
 const kv = await Deno.openKv();
 
 //セット
-const pw = await kv.get(['dog', 'いぬ'], {power: 10, speed: 5});
+const pw = await kv.set(['dog', 'いぬ'], {power: 10, speed: 5});
 
 //ゲット
 const dog = await kv.get(['dog', 'いぬ']);
-console.log('power:' + dog.value);
 console.log('value:' + JSON.stringify(dog.value))
 console.log('key:' + dog.key);
-console.log(dog.value, dog.key);
+console.log(dog.key, dog.value);
 
-const kerai = await kv.get(['setvant'])
-console.log('dog:' + JSON.stringify(kerai.value))
+
+await kv.set(['bird', '鳥'], {power: 3, speed: 8});
+const bird = await kv.get(['bird', '鳥']);
+
+ const animal = await kv.get(['animal'])
+ console.log('dog:' + JSON.stringify(animal.value))
+
+//リスト
+const animalList = await kv.list({prefix: ['animal']});
+for await (const animal of animalList){
+    console.log(animal.key);
+    console.log(animal.value);
+    List.push(animal.value);
+}
+
+
+//削除
+await kv.set(['cat', '猫'], {power: 5, speed: 6});
+let animals = await kv.get(['cat', '猫']);
+console.log(animals.key, animals.value);
+
+await kv.delete(['cat', '猫']);
+
+animals = await kv.get(['cat', '猫']);
+console.log(animals.value);
